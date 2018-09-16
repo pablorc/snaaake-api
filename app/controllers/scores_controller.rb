8b161c294e0d3bf1a46fe7a3a5b86be34c8c8a08
Pablo Rivera Conde <pablo.rivera@marsbased.com>
2018-09-16 19:39:06 +0200

    Return global highscores on score create

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,17 +1,30 @@
 class ScoresController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  NUMBER_OF_RESULTS = 10
+
+  def index
+    @scores = highscores
+  end
+
   def create
     @score = Score.new(score_params)
 
     if @score.save
-      render json: 'OK'.to_json
+      render json: {
+        result: 'OK',
+        highscores: highscores.map { |score| [score.author, score.value] }
+      }.to_json
     else
       render json: 'KO'.to_json
     end
   end
 
   private
+
+  def highscores
+    Score.limit(NUMBER_OF_RESULTS)
+  end
 
 
   def score_params
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,3 +1,5 @@
 class Score < ApplicationRecord
   validates_presence_of :value, :author
+
+  default_scope { order(value: :desc) }
 end
